303e5fe761ecdb5c0af8bdd8d8ece4263b39e758
Dominic <lovric.domagoj1@gmail.com>
2023-11-21 19:32:33 +0000

    COlorscheme added

--- lua/dlovric/packer.lua
+++ lua/dlovric/packer.lua
@@ -12,9 +12,9 @@ return require('packer').startup(function(use)
 	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
-  
-  -- Colorscheme 
-  require('packer').startup(function(use)
-	  use({ 'rose-pine/neovim', as = 'rose-pine' })
-  end)
+
+  -- Colorscheme for the nvim
+  use({ 'rose-pine/neovim', as = 'rose-pine', config = function()
+	  vim.cmd('colorscheme rose-pine') end})
+
   end)
