f7028d3fd2b8086fa390d029c96fbadb274139ab
Dominic <lovric.domagoj1@gmail.com>
2023-11-21 19:50:29 +0000

    TReesitter setup for syntax highlight

--- lua/dlovric/packer.lua
+++ lua/dlovric/packer.lua
@@ -5,16 +5,27 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
-
   -- Telescope for fuzzy find and live grep
   use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.4',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
+    'nvim-telescope/telescope.nvim', 
+    tag = '0.1.4',
+    -- or                            , branch = '0.1.x',
+    requires = { {'nvim-lua/plenary.nvim'} }
   }
 
   -- Colorscheme for the nvim
-  use({ 'rose-pine/neovim', as = 'rose-pine', config = function()
-	  vim.cmd('colorscheme rose-pine') end})
+  use { 
+    'rose-pine/neovim', 
+    as = 'rose-pine', 
+    config = function()
+      vim.cmd('colorscheme rose-pine') 
+    end
+  }
 
-  end)
+  -- Treesitter for syntax parsing
+  use ( 
+    'nvim-treesitter/nvim-treesitter',
+    {run = ':TSUpdate'}
+    )
+
+end)
