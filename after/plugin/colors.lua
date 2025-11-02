24f99f829b78dfce20cdabbe9fd87497602e48e7
DL <lovric.domagoj1@gmail.com>
2025-11-02 15:32:40 +0000

    after packersync the color of rose-pine have changed, so moving to rose-pine-moon

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,9 +1,22 @@
 require('rose-pine').setup({
-    disable_background = true
+    variant = 'main',  -- main, moon, or dawn
+    dark_variant = 'main',
+    disable_background = true,
+
+    highlight_groups = {
+        -- Make sure syntax is visible
+        ColorColumn = { bg = 'rose' },
+
+        -- Ensure these are bright enough
+        String = { fg = 'foam', italic = true },
+        Function = { fg = 'love' },
+        Keyword = { fg = 'pine' },
+        Comment = { fg = 'muted' },
+    },
 })
 
-function ColorMyPencils(color) 
-	color = color or "rose-pine"
+function ColorMyPencils(color)
+	color = color or "rose-pine-moon"  -- Using moon variant which is brighter
 	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
